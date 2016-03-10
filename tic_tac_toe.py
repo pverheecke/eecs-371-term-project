645301fe81a0533c58c748bd8f8bc34cb22a4587
pverheecke <pverheecke@gmail.com>
2016-03-10 14:34:18 -0600

    final version of tic_tac_toe with minimax implemented

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,6 +13,9 @@ win_indices = ((0,1,2), (3,4,5), (6,7,8),
         (0,3,6), (1,4,7), (2,5,8),
         (0,4,8), (2,4,6))
 
+# Global variable for computer's choice
+computer_choice = None
+
 
 def print_board():
     """
@@ -23,42 +26,90 @@ def print_board():
     print '\n'
 
 
-def check_winner():
+def check_winner(board=curr_board):
     """
     Returns the (winner, indices of winning squares) if there is one.
     Returns (None, None) if there is no winner yet
     """
     for win in win_indices:
-        b = curr_board[win[0]]
-        if b in 'XO' and all (curr_board[i] == b for i in win):
+        b = board[win[0]]
+        if b in 'XO' and all (board[i] == b for i in win):
             return b, [i+1 for i in win]
     return None, None
 
 
-def draw():
+def draw(board=curr_board):
     """
     Returns whether or not the game is at a draw.
     """
-    return all (b in 'XO' for b in curr_board)
+    return all (b in 'XO' for b in board)
 
 
-def get_open_spaces():
+def get_open_spaces(board=curr_board):
     """
     Returns the number-chars representing empty/available
     squares on the gameboard.
     """
-    return [b for b in curr_board if b not in 'XO']
+    return [b for b in board if b not in 'XO']
 
 
-def computer_turn_do(xo):
+def game_over(board):
     """
-    Performs a random move.
-    Replace this with minimax AI decision.
+    Checks whether or not the game has ended in some way.
     """
-    options = get_open_spaces()
-    choice = random.choice(options)
-    curr_board[int(choice)-1] = xo
-    return choice
+    return check_winner(board=board)[0] or draw(board=board)
+
+
+def get_possible_boards(board, moves, player):
+    """
+    Returns a list of possible board layouts, given the player
+    whose turn it is, and a list of valid moves.
+    """
+    boards = []
+    for move in moves:
+        new_board = board[:]
+        new_board[int(move)-1] = player
+        boards.append(new_board)
+    return boards
+
+
+def minimax_score(board):
+    """
+    Evaluation function that assigns a score to some ending board
+    layout.
+    """
+    winner = check_winner(board=board)
+    if winner[0]:
+        if winner[0] == computer:
+            return 10
+        else:
+            return -10
+    else:
+        return 0
+
+def minimax(board, curr_player, other_player):
+    """
+    Calculates the best move for the computer, based on building
+    a tree of all possible ways the remainder of the game can go.
+    Chooses the options with the best outcome, i.e. where the
+    computer has the lowest chance to lose.
+    """
+    global computer_choice
+    if game_over(board):
+        return minimax_score(board)
+    scores = []
+    moves = get_open_spaces(board=board)
+    new_boards = get_possible_boards(board, moves, curr_player)
+    for boards in new_boards:
+        scores.append(minimax(boards, other_player, curr_player))
+    if curr_player == computer:
+        max_index = scores.index(max(scores))
+        computer_choice = moves[max_index]
+        return scores[max_index]
+    else:
+        min_index = scores.index(min(scores))
+        computer_choice = moves[min_index]
+        return scores[min_index]
 
 
 def player_turn_do(xo):
@@ -86,7 +137,11 @@ def computer_turn(xo):
     the move after it's made.
     """
     print_board()
-    choice = computer_turn_do(xo)
+    global minimax_depth
+    minimax_depth = 0
+    turn_score = minimax(curr_board, computer, player)
+    choice = computer_choice
+    curr_board[int(choice)-1] = computer
     print 'The computer placed an %s at %s' % (computer, choice)
     return check_winner()
 
@@ -115,6 +170,8 @@ if player == 'O':
     computer = 'X'
 else:
     computer = 'O'
+
+
 
 
 # Gameplay loop
